3f24ff023545d537c37ccb281b08602dc662b492
Jason Le <36774043+Jasonleh99@users.noreply.github.com>
2019-02-16 03:40:00 -0800

    Added client id

--- public/js/releaseForm.js
+++ public/js/releaseForm.js
@@ -1,7 +1,8 @@
 <script type="text/javascript" src="https://s3.amazonaws.com/cdn.hellosign.com/public/js/hellosign-embedded.LATEST.min.js"></script>
 const hellosign = require('hellosign-sdk')({ key: '5c06ce44f3fe798ba5cbfaae4ac8ddedc8b176a0a846b26e4b9513bb2c271b3e' });
+var clientID = '5c4de85db6346506547adfc62691f36f';
 
-function sendForm(clientID, photographerJson, modelJson){
+function sendForm(photographerJson, modelJson){
   var photographer = JSON.parse(photographerJson);
   var model = JSON.parse(modelJson);
   
@@ -37,7 +38,7 @@ function sendForm(clientID, photographerJson, modelJson){
   });
 }
 
-function openDoc(clientID, signURL){
+function openDoc(signURL){
   HelloSign.init(clientID);
   HelloSign.open({
       url: signURL,
