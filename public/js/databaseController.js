fba96d4558dea63a7680d203b0af14133aa3cb30
Jason Le <36774043+Jasonleh99@users.noreply.github.com>
2019-02-16 16:22:15 -0800

    installed firebase

---
+++ public/js/databaseController.js
@@ -0,0 +1,27 @@
+var firebaseRef = "https://photosign-fdb51.firebaseio.com";
+var storageRef = firebase.storage().ref();
+
+/*
+* name: name
+* email: email
+* location: location
+* photographer: false
+* model: false
+* seekingPhotographer: false
+* seekingModel: false
+*/
+function storeProfile(profileJson){
+    
+}
+/*
+* photo: base64Image
+* owner: email
+* description: description
+* location: location
+* modeling: false
+* photography: false
+* taggedUsers: [email, email, email, email, email]
+*/
+function storeImage(email, base64Image){
+
+}
